5dd1cd748a91a164023ea71556f9e13c7ddf827b
R <vr757@apac.comcast.com>
2025-01-12 18:23:33 +0530

    code updated 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Next Change in VM '
+    return 'Hello, Next Change in VM | v2 '
 
 if __name__ == '__main__':
     app.run(debug=True)
